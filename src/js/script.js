0ab1770ccfd23c75eb5c28be51ab2efb57642dac
Tumemo <hugo.f.silva15@aluno.senai.br>
2025-11-24 15:44:12 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const section = document.getElementById('FreqCompOnda')
+const luzvis = document.querySelector(".luzvis")
+
+luzvis.addEventListener('click', ()=>{
+    section.classList.toggle("visivel")
+})
